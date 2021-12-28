41853ec3a1c0edbb84a87da09b1d08bf5ef14679
Baescott <kibae92@gmail.com>
2021-12-28 11:13:23 +0900

    reformat code according to PEP8 and add explanations

--- get_course_info.py
+++ get_course_info.py
@@ -5,27 +5,31 @@ import json
 import urllib.request
 
 if __name__ == "__main__":
-    client = None
-    with open("./google_key.json", "r") as clientJson:
-        client = json.load(clientJson)
+    # Read GCP key (be private!)
+    with open("./google_key.json", "r") as client_json:
+        client = json.load(client_json)
 
+    # Make inputs
+    # - `origin` and `destination` will be replaced to sys.argv
     origin = "37.565211608460935,126.90448920622298"
     destination = "37.571270658174626,126.9348124477444"
     mode = "transit"
     departure_time = "now"
     key = client["key"]
 
+    # Make API url with inputs
     url = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}" + \
           f"&destination={destination}" + \
           f"&mode={mode}" + \
           f"&departure_time={departure_time}" + \
           f"&language=ko&key={key}"
 
+    # Download course result as json file
     request = urllib.request.Request(url)
     context = ssl._create_unverified_context()
     response = urllib.request.urlopen(request, context=context)
-    responseText = response.read().decode('utf-8')
-    responseJson = json.loads(responseText)
+    response_text = response.read().decode('utf-8')
+    response_json = json.loads(response_text)
 
     with open("./Agent_Transit_Directions.json", "w") as rltStream:
-        json.dump(responseJson, rltStream)
+        json.dump(response_json, rltStream)
--- print_course_info.py
+++ print_course_info.py
@@ -1,25 +1,29 @@
 import json
 
 if __name__ == "__main__":
-    wholeDict = None
-    with open("./Agent_Transit_Directions.json", "r") as transitJson:
-        wholeDict = dict(json.load(transitJson))
+    # Read json formatted course file
+    with open("./Agent_Transit_Directions.json", "r") as transit_json:
+        whole_dict = dict(json.load(transit_json))
 
-    path = wholeDict["routes"][0]["legs"][0]
+    # Get necessary values from result json file
+    path = whole_dict["routes"][0]["legs"][0]
     duration_sec = path["duration"]["value"]
     start_geo = path["start_location"]
     end_geo = path["end_location"]
 
+    # Print results
+    # - This will be replaced to ETL procedure soon
     print("우리 집으로부터 윰네 파라다이스텔 SB 까지의 거리다!!!!!!!!!!!!!!")
     print(duration_sec, "초가 걸린다!!!!!!!!!!!")
     print(start_geo)
     print(end_geo)
-    stepList = path["steps"]
+    step_list = path["steps"]
 
-    print("경로는 총", len(stepList), "개다!!!!!!!!!!!")
+    print("경로는 총", len(step_list), "개다!!!!!!!!!!!")
     print("경로는 다음과 같다!!!!!!!!!!!!")
     print("-------------------------")
     step = 0
-while step < len(stepList):
-    print(stepList[step])
+
+while step < len(step_list):
+    print(step_list[step])
     step = step + 1
