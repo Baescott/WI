8da6ae76eaff5f5ee05c8c331286503a43dd1935
Mingyu Yang <yang926@naver.com>
2021-12-29 17:29:22 +0900

    경어체로 수정

--- get_course/print_course_info.py
+++ get_course/print_course_info.py
@@ -13,15 +13,16 @@ if __name__ == "__main__":
 
     # Print results
     # - This will be replaced to ETL procedure soon
-    print("우리 집으로부터 윰네 파라다이스텔 SB 까지의 거리다!!!!!!!!!!!!!!")
-    print(duration_sec, "초가 걸린다!!!!!!!!!!!")
+    print("상암미르웰한올림","부터","파라다이스텔SB","까지의 경로")
+    print(duration_sec, "초가 걸립니다.")
     print(start_geo)
     print(end_geo)
     step_list = path["steps"]
 
-    print("경로는 총", len(step_list), "개다!!!!!!!!!!!")
-    print("경로는 다음과 같다!!!!!!!!!!!!")
+    print("경로는 총", len(step_list), "번의 과정이 있으며,")
+    print("경로는 다음과 같습니다.")
     print("-------------------------")
+    
     step = 0
 
 while step < len(step_list):
